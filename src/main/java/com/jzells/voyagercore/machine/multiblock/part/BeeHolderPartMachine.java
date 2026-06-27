6120c1262179fb3f5ccf55ba3b72e716ced39746
TeusTF <207483626+TeusTF@users.noreply.github.com>
2026-06-26 22:59:28 -0500

    Create BeeHolderPartMachine.java
    Test file, ignore

---
+++ src/main/java/com/jzells/voyagercore/machine/multiblock/part/BeeHolderPartMachine.java
@@ -0,0 +1,11 @@
+package com.jzells.voyagercore.machine.multiblock.part;
+
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.multiblock.part.MultiblockPartMachine;
+
+public class BeeHolderPartMachine extends MultiblockPartMachine {
+
+    public BeeHolderPartMachine(IMachineBlockEntity holder) {
+        super(holder);
+    }
+}
