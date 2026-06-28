17703834a718af69cd922aa5040467e1eaf3d245
TeusTF <207483626+TeusTF@users.noreply.github.com>
2026-06-27 19:30:06 -0500

    Moved Stuff
    Setup is a pain

--- src/main/java/com/jzells/voyagercore/common/data/machines/VoyagerMachines.java
+++
@@ -1,5 +0,0 @@
-package com.jzells.voyagercore.common.data.machines;
-
-public class VoyagerMachines {
-
-}
--- src/main/java/com/jzells/voyagercore/common/machine/multiblock/VoyagerMultiRegistry.java
+++ src/main/java/com/jzells/voyagercore/common/machine/multiblock/VoyagerMultiRegistry.java
@@ -1,10 +1,12 @@
 package com.jzells.voyagercore.common.machine.multiblock;
 
+import com.jzells.voyagercore.common.machine.multiblock.electric.ElectricMultiMachines;
 import com.jzells.voyagercore.common.machine.multiblock.generator.calorieconverters.CalorieConverterMultis;
 
 public class VoyagerMultiRegistry {
 
     public static void init() {
         CalorieConverterMultis.init();
+        ElectricMultiMachines.init();
     }
 }
