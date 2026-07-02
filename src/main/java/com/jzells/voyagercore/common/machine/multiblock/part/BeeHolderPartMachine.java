d21339bc6b21f436cac761b05b41e8c56e83d571
TeusTF <207483626+TeusTF@users.noreply.github.com>
2026-07-01 20:00:54 -0500

    GRADLE
    This build system can bite me

--- src/main/java/com/jzells/voyagercore/common/machine/multiblock/part/BeeHolderPartMachine.java
+++ src/main/java/com/jzells/voyagercore/common/machine/multiblock/part/BeeHolderPartMachine.java
@@ -3,10 +3,11 @@ package com.jzells.voyagercore.common.machine.multiblock.part;
 import com.gregtechceu.gtceu.api.capability.IControllable;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.feature.IInteractedMachine;
 import com.gregtechceu.gtceu.api.machine.feature.IMachineLife;
 import com.gregtechceu.gtceu.api.machine.multiblock.part.MultiblockPartMachine;
 
-public class BeeHolderPartMachine extends MultiblockPartMachine implements IControllable, IMachineLife {
+public class BeeHolderPartMachine extends MultiblockPartMachine implements IControllable, IMachineLife, IInteractedMachine {
 
     protected final IO io;
     protected boolean workingEnabled;
