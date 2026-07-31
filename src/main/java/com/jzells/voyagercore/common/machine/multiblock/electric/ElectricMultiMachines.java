7f7fd2a71871aa7be554a3d61b28e9f3e30d07c6
TeusTF <207483626+TeusTF@users.noreply.github.com>
2026-07-31 14:50:22 -0500

    Updated module() Predicate
    Added MachineDefiniton to the args, allowing for filtering of modules.
    
    Currently forms without stating issues, trying to find a work around

--- src/main/java/com/jzells/voyagercore/common/machine/multiblock/electric/ElectricMultiMachines.java
+++ src/main/java/com/jzells/voyagercore/common/machine/multiblock/electric/ElectricMultiMachines.java
@@ -483,24 +483,6 @@ public class ElectricMultiMachines {
                     VoyagerCore.id("block/multiblock/helper_coiltronics_assembly"))
             .register();
 
-    public static final MultiblockMachineDefinition TEST_MODULAR = VOYAGERCORE_REGISTRATE
-            .multiblock("test_modular_multi", ModularMachine::new)
-            .rotationState(RotationState.ALL)
-            .recipeType(GTRecipeTypes.DUMMY_RECIPES)
-            .appearanceBlock(CASING_INDUSTRIAL_SOLID)
-            .pattern(def -> FactoryBlockPattern.start()
-                    .aisle("   ", " M ", "   ")
-                    .aisle("XXX", "XXX", "XXX")
-                    .aisle("XXX", "XCX", "XXX")
-                    .where('X', Predicates.blocks(CASING_INDUSTRIAL_SOLID.get()))
-                    .where(' ', Predicates.any())
-                    .where('M', VoyagerPredicates.module())
-                    .where('C', Predicates.controller(Predicates.blocks(def.get())))
-                    .build())
-            .workableCasingModel(VoyagerCore.id("block/casing/solid_industrial_casing"),
-                    VoyagerCore.id("block/multiblock/helper_coiltronics_assembly"))
-            .register();
-
     public static final MultiblockMachineDefinition TEST_MODULE = VOYAGERCORE_REGISTRATE
             .multiblock("test_module", ModuleInstanceMachine::new)
             .rotationState(RotationState.ALL)
@@ -515,9 +497,41 @@ public class ElectricMultiMachines {
                     VoyagerCore.id("block/multiblock/helper_coiltronics_assembly"))
             .register();
 
+    public static final MultiblockMachineDefinition TEST_NOT_MODULE = VOYAGERCORE_REGISTRATE
+            .multiblock("test_not_module", ModuleInstanceMachine::new)
+            .rotationState(RotationState.ALL)
+            .recipeType(GTRecipeTypes.DUMMY_RECIPES)
+            .appearanceBlock(CASING_INDUSTRIAL_SOLID)
+            .pattern(def -> FactoryBlockPattern.start()
+                    .aisle("XCX")
+                    .where('X', Predicates.blocks(CASING_INDUSTRIAL_SOLID.get()))
+                    .where('C', Predicates.controller(Predicates.blocks(def.get())))
+                    .build())
+            .workableCasingModel(VoyagerCore.id("block/casing/solid_industrial_casing"),
+                    VoyagerCore.id("block/multiblock/helper_coiltronics_assembly"))
+            .register();
+
+    public static final MultiblockMachineDefinition TEST_MODULAR = VOYAGERCORE_REGISTRATE
+            .multiblock("test_modular_multi", ModularMachine::new)
+            .rotationState(RotationState.ALL)
+            .recipeType(GTRecipeTypes.DUMMY_RECIPES)
+            .appearanceBlock(CASING_INDUSTRIAL_SOLID)
+            .pattern(def -> FactoryBlockPattern.start()
+                    .aisle("   ", " M ", "   ")
+                    .aisle("XXX", "XXX", "XXX")
+                    .aisle("XXX", "XCX", "XXX")
+                    .where('X', Predicates.blocks(CASING_INDUSTRIAL_SOLID.get()))
+                    .where(' ', Predicates.any())
+                    .where('M', VoyagerPredicates.module(TEST_MODULE))
+                    .where('C', Predicates.controller(Predicates.blocks(def.get())))
+                    .build())
+            .workableCasingModel(VoyagerCore.id("block/casing/solid_industrial_casing"),
+                    VoyagerCore.id("block/multiblock/helper_coiltronics_assembly"))
+            .register();
     // ====================//
     // Spotless is disabled past this point
     // spotless:off
     // ====================//
+
     public static void init() {}
 }
